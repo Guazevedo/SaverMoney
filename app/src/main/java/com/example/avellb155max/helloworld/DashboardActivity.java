5f588eb9453a34b1a21db4908c807111ae8d646f
Guazevedo <gu.ve.13@gmail.com>
2017-06-07 00:33:32 -0300

    3 vez pagina excluir adicionada a dashboard

--- app/src/main/java/com/example/avellb155max/helloworld/DashboardActivity.java
+++ app/src/main/java/com/example/avellb155max/helloworld/DashboardActivity.java
@@ -18,10 +18,11 @@ public class DashboardActivity extends AppCompatActivity {
             startActivity(new Intent(this,NovoGasto.class));
         }
     public void consulta(View view){
-
         startActivity(new Intent(this,Consulta.class));
-
-
     }
+    public void excluir(View view){
+        startActivity(new Intent(this,excluir.class));
+    }
+
 
 }
