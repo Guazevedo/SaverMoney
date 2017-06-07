b8171c1ebd2ac30aadb2b026a94ca3b20b05be2b
Guazevedo <gu.ve.13@gmail.com>
2017-06-06 22:51:14 -0300

    1 vez

---
+++ app/src/main/java/com/example/avellb155max/helloworld/DashboardActivity.java
@@ -0,0 +1,27 @@
+package com.example.avellb155max.helloworld;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+    public void novoGasto(View view){
+            startActivity(new Intent(this,NovoGasto.class));
+        }
+    public void consulta(View view){
+
+        startActivity(new Intent(this,Consulta.class));
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/avellb155max/helloworld/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.avellb155max.helloworld;
+
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText usuario;
+    private EditText senha;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main);
+        usuario= (EditText) findViewById(R.id.login);
+        senha= (EditText) findViewById(R.id.senha);
+        Button submit= (Button)  findViewById(R.id.btnLogin);
+
+    }
+
+    public void entraOnClick(View view){
+        String usuarioInf =usuario.getText().toString();
+        String senhaInf =senha.getText().toString();
+
+        if("adm".equals(usuarioInf)&&"123".equals(senhaInf)){
+            startActivity(new Intent(this,DashboardActivity.class));
+        } else {
+
+            String mensagemErro= getString(R.string.erroAut);
+            Toast toast= Toast.makeText(this,mensagemErro,Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+    }
+
+
+}
